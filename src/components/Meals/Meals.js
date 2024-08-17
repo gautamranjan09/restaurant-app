a357251c3570eda7a018e9f7b0400a12ebe24b1b
gautamranjan09 <gautamranjan96@gmail.com>
2024-08-17 18:22:27 +0530

    Added restaurant summary component to enhance user experience with key details and highlights of the restaurant.

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MealsSummary from './MealsSummary';
+import AvailableMeal from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <>
+    <MealsSummary/>
+    <AvailableMeal/>
+    </>
+  );
+}
+
+export default Meals;
