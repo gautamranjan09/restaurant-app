8cfad7e56eca1ccdadefc4f524b354fc2978c78b
gautamranjan09 <gautamranjan96@gmail.com>
2024-08-21 16:32:02 +0530

    Implement comprehensive cart functionality
    - Added logic for adding, removing, and updating items in the cart.
    - Utilized `useContext` to manage global cart state across the application.
    - Implemented `useRef` for referencing DOM elements and optimizing performance.
    - Used `useReducer` to handle complex cart state logic and actions efficiently.
    - Enhanced user experience with dynamic updates to the cart, ensuring that all operations are seamless and responsive.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown]=useState(false);
@@ -13,15 +14,15 @@ function App() {
   const hideCartHandler=()=>{
     setCartIsShown(false);
   };
-
+  
   return (
-    <>
+    <CartProvider>
     {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals/>
       </main>
-    </>
+    </CartProvider>
   );
 }
 
